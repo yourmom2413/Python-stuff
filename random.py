bf40150bd77e12f2c12c990a678daf1a780eee53
Audrey <90288662+yourmom2413@users.noreply.github.com>
2021-10-14 20:59:50 -0400

    Update

--- random.py
+++ random.py
@@ -1,2 +1,12 @@
-input ("hello how are you")
-if == print ("that is good") 
+Answer = input ("hello how was your day?: ").lower()
+if Answer == "good":
+    input ("Why was it good? ")
+elif Answer == "bad":
+    input ("Why was it bad? ")
+elif Answer == "fine":
+    input ("Why was it fine? ")
+elif Answer == "okay" or Answer == "ok":
+    input ("Why was your day okay? ")
+
+def func1():
+    print
