ce204e9be4d16d33786d53e60335845061b05d71
Audrey <90288662+yourmom2413@users.noreply.github.com>
2021-10-14 22:49:03 -0400

    Create jacktask.py

---
+++ jacktask.py
@@ -0,0 +1,24 @@
+from colorama import Fore
+import colorama
+
+Verb = input ("Give me one Verb: ")
+Noun = input ("Give me one Noun: ")
+Adjective = input ("Give me one Adjective: ")
+Adjective2 = input ("Give me another Adjective: ")
+Adverb = input ("Give me an Adverb: ")
+Noun2 = input ("Give me another Noun: ")
+Noun3 = input ("Give me one more Noun: ")
+Adverb2 = input ("Give me another Adverb: ")
+
+
+print (Fore.BLUE + "\nSummer is all about the beach! During the daytime, you can " + Verb)
+print ("In the lake or sea and collect seashells and " + Noun + " Then feast on")
+print ("a meal of " + Adjective + " hot dogs and " + Adjective2 + " ice cream.")
+print ("They're " + Adverb + " yummy. After that, rest on a boat as you catch")
+print ("some of the sun's bright rays. As the sunlight slowly slips away, you can")
+print ("rest and view the nightime sky. You might see " + Noun2 + " and " + Noun3 + ".")
+print ("All thats left to do is whisper, Good night. A day at the beach is " + Adverb2)
+print ("a treat-- unless it rains, of course!" + Style.RESET_ALL)
+
+
+
